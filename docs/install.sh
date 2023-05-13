8b6daeb7dfeda73b66c5996156f981afa00194a7
Gianpaolo Macario <gianpaolo.macario@solarma.it>
2023-05-13 20:20:48 +0200

    Update install.sh and test.sh
    From Revision 176 of
    https://gist.github.com/gmacario/60d2b898395eb40c693c7105d9520435/revisions

--- docs/install.sh
+++ docs/install.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-set -e
-# set -x
+set -xe
 
-echo "TODO"
+# Update OS
+sudo apt-get update \
+  && sudo apt-get -y dist-upgrade \
+  && sudo apt-get -y autoremove --purge
+
+# Install missing packages (if any)
+which bluetoothctl || sudo apt-get -y install bluez
+which expect       || sudo apt-get -y install expect
+which lsb_release  || sudo apt-get -y install lsb-release
+which lsof         || sudo apt-get -y install lsof
+which lsusb        || sudo apt-get -y install usbutils
+
+# Check OS and kernel version
+uname -a
+lsb_release -a
+
+# Check BlueZ installed version
+dpkg -l bluez
+
+# Check installed Bluetooth interfaces
+hciconfig -a
 
 # EOF
