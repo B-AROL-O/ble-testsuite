4d5fe1a2b6dc9318f52d54dc8d9880f0f38c7b4d
Gianpaolo Macario <gianpaolo.macario@solarma.it>
2023-05-14 22:11:23 +0200

    Successfully created a workflow dispatch event

--- test-github-webhook.sh
+++ test-github-webhook.sh
@@ -5,6 +5,7 @@
 # References:
 # * https://docs.github.com/en/actions/learn-github-actions/understanding-github-actions
 # * https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#create-a-repository-dispatch-event
+# * https://www.enekoalonso.com/articles/issue-7
 #
 # HOW TO USE:
 # * Open <https://github.com/settings/personal-access-tokens/new>
@@ -18,7 +19,8 @@
 #   * Permissions:
 #     * Repository permissions:
 #       * Actions: Access: Read and write
-#       * Administration: TODO
+#       * ...
+#       * Contents: Access: Read and write
 #       * ...
 #       * Workflows: Access: Read and write
 # * Review summary in section "Overview", then click "Generate token"
@@ -30,20 +32,31 @@ set -e
 set -x
 
 # MY_TOKEN=github_pat_xxxx
-MY_TOKEN=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
+MY_TOKEN=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
 OWNER="gmacario"
 REPO="ble-testsuite"
+WORKFLOW_ID="test-webhook.yml"
 PAYLOAD="${PWD}/docs/sample_webhooks/2023-05-11-120942-webhook-site.json"
 
-# Create a repository dispatch event
-# ENDPOINT_URL="https://api.github.com/repos/${OWNER}/${REPO}/dispatches"
-cat ${PAYLOAD} | curl -L \
+# Create a workflow dispatch event
+# https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
+# POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches
+curl -L \
   -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${MY_TOKEN}"\
+  -H "Authorization: Bearer ${MY_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/${OWNER}/${REPO}/dispatches \
-  -d '{"event_type":"on-demand-test","client_payload":{"unit":false,"integration":true}}'
+  https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches \
+  -d '{"ref":"main","inputs":{}}'
+
+# # Create a repository dispatch event
+# # ENDPOINT_URL="https://api.github.com/repos/${OWNER}/${REPO}/dispatches"
+# cat ${PAYLOAD} | curl -L \
+#   -X POST \
+#   -H "Accept: application/vnd.github+json" \
+#   -H "Authorization: Bearer ${MY_TOKEN}"\
+#   -H "X-GitHub-Api-Version: 2022-11-28" \
+#   https://api.github.com/repos/${OWNER}/${REPO}/dispatches \
+#   -d '{"event_type":"on-demand-test","client_payload":{"unit":false,"integration":true}}'
 
 # TODO
 
