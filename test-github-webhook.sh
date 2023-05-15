ad7d89776ce4c67af868edf94827206189bf1b89
Macario Gianpaolo <gianpaolo.macario@arol.com>
2023-05-15 12:28:55 +0200

    Escaping double quotes works with simple.json

--- test-github-webhook.sh
+++ test-github-webhook.sh
@@ -36,7 +36,13 @@ MY_TOKEN=github_pat_11AAASLLQ0g8tn3JhOuUKj_nRfgKtg2Oh11PIhWOHlhqb9CcWC7amg79jMaV
 OWNER="gmacario"
 REPO="ble-testsuite"
 WORKFLOW_ID="test-webhook.yml"
-PAYLOAD="${PWD}/docs/sample_webhooks/2023-05-11-120942-webhook-site.json"
+PAYLOAD_FILE="${PWD}/docs/sample_webhooks/2023-05-11-120942-webhook-site.json"
+
+PAYLOAD_FILE=simple.json
+# PAYLOAD="$(cat $PAYLOAD_FILE | sed 's/\"/''/g' | tr -d '\n')"
+# PAYLOAD="$(cat $PAYLOAD_FILE | sed 's/\"/''/g' | tr -d '\n')"
+PAYLOAD="$(cat $PAYLOAD_FILE | sed 's/\"/\\\"/g' | tr -d '\n')"
+echo "DEBUG: PAYLOAD=$PAYLOAD"
 
 # Create a workflow dispatch event
 # https://docs.github.com/en/rest/actions/workflows?apiVersion=2022-11-28#create-a-workflow-dispatch-event
@@ -46,10 +52,17 @@ curl -L \
   -H "Authorization: Bearer ${MY_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/${OWNER}/${REPO}/actions/workflows/${WORKFLOW_ID}/dispatches \
-  -d '{"ref":"feat/bitbucket-webhook","inputs":{"unit":false, "integration":false, "payload":"my_payload"}'
+  -d "{\"ref\":\"feat/bitbucket-webhook\",\"inputs\":{\"unit\":\"u2\",\"integration\":\"false\",\"payload\":\"$PAYLOAD\"}"
 
-# -d '{"ref":"main","inputs":{"unit":false, "integration":{}}}'
-  # "unit":false,"integration":true
+# TODO
+
+# \"$PAYLOAD\"
+
+# Examples of valid contents for the workflow_dispatch API:
+#   -d '{"ref":"feat/bitbucket-webhook","inputs":{"unit":"u2", "integration":false, "payload":"{a:b}"}'
+#   -d '{"ref":"feat/bitbucket-webhook","inputs":{"unit":"u2", "integration":false, "payload":"my_payload"}'
+#   -d '{"ref":"feat/bitbucket-webhook","inputs":{"unit":"u2", "integration":"false", "payload":"my_payload"}'
+#   -d '{"ref":"main","inputs":{"unit":false, "integration":{}}}'
 
 # # Create a repository dispatch event
 # # ENDPOINT_URL="https://api.github.com/repos/${OWNER}/${REPO}/dispatches"
