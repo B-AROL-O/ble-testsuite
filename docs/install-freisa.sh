eadc44afd38991ca006ad3bc0d150c428f720a5e
Gianpaolo Macario <gianpaolo.macario@solarma.it>
2023-09-18 22:32:58 +0200

    Minor improvements on check if Docker installed

--- docs/install-freisa.sh
+++ docs/install-freisa.sh
@@ -29,15 +29,17 @@ hostname -I
 # Check installed Bluetooth interfaces
 hciconfig -a
 
-# Install Docker Engine using the convenience script
-# See https://docs.docker.com/engine/install/ubuntu/
-if (which docker); then
+# Check if Docker is already installed
+if (which docker &>/dev/null); then
   echo "INFO: Docker is already installed"
   docker --version
 else
+  # Install Docker Engine using the convenience script
+  # See https://docs.docker.com/engine/install/ubuntu/
   curl -fsSL https://get.docker.com -o get-docker.sh
   sudo sh ./get-docker.sh --dry-run
   sudo sh ./get-docker.sh
+  rm -f ./get-docker.sh
 fi
 
 # EOF
